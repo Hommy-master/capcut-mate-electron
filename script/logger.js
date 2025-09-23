e824df4fe586e175c808939c9c4308e1f54f4535
张琼娜 <gogoshine168@gmail.com>
2025-09-23 23:36:07 +0800

    动态的文件不能使用 __dirname 路径，具体原因待查

--- script/logger.js
+++ script/logger.js
@@ -1,13 +1,14 @@
 // logger.js
 const log4js = require('log4js');
 const path = require('path');
+const { app } = require('electron');
 
 log4js.configure({
     appenders: {
         // 定义一个按日期分割的日志文件追加器
         dateFile: {
             type: 'dateFile',
-            filename: path.join(__dirname, '..', 'logs', 'app.log'), // 日志文件路径
+            filename: path.join(app.getPath('userData'), 'logs', 'app.log'), // 日志文件路径
             pattern: 'yyyy-MM-dd', // 按天分割
             compress: false, // 是否压缩备份文件
             keepFileExt: true, // 保持.log扩展名
